c38372c6df86621565b0e4dfc466f81ddf5a74f7
Gulnihal <gh.naldoken@gmail.com>
2021-07-06 13:04:42 +0100

    F - I used map

--- 1-exercises/F-array-forEach/exercise.js
+++ 1-exercises/F-array-forEach/exercise.js
@@ -9,6 +9,22 @@
 
 var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
+const newArr = arr.map(number => {
+
+  if ( number % 15 === 0){
+    return "FizzBuzz";
+  } else if ( number % 5 === 0){
+    return "Buzz";
+  }else if (number % 3 === 0){
+    return "Fizz";
+  }else {
+    return number;
+  }
+});
+
+console.log(newArr);
+console.log(arr); //<---------- original array
+
 /* EXPECTED OUTPUT */
 
 /*
